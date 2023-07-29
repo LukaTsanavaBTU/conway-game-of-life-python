ee570df97274d90c68e6bdebe7663c4eb1d57800
LukaTsanavaBTU <115739463+LukaTsanavaBTU@users.noreply.github.com>
2023-07-29 09:32:59 +0400

    Update game_of_life.py

--- game_of_life.py
+++ game_of_life.py
@@ -9,18 +9,20 @@ space = np.zeros((height, width))
 
 
 def space_cycle():
-    to_activate = []
-    to_deactivate = []
-    for i in range(0, height):
-        for j in range(0, width):
-            if space[i, j] == 1:
-                to_activate += check_cells_around(i, j)
-                if check_around(i, j) not in (2, 3):
-                    to_deactivate += [(i, j)]
-    for cell in to_activate:
-        space[cell[0], cell[1]] = 1
-    for cell in to_deactivate:
-        space[cell[0], cell[1]] = 0
+    global paused
+    if not paused:
+        to_activate = []
+        to_deactivate = []
+        for i in range(0, height):
+            for j in range(0, width):
+                if space[i, j] == 1:
+                    to_activate += check_cells_around(i, j)
+                    if check_around(i, j) not in (2, 3):
+                        to_deactivate += [(i, j)]
+        for cell in to_activate:
+            space[cell[0], cell[1]] = 1
+        for cell in to_deactivate:
+            space[cell[0], cell[1]] = 0
 
 
 def check_around(i, j):
@@ -50,7 +52,24 @@ def update(frames):
     return im,
 
 
+def onclick(event):
+    if event.inaxes is not None:
+        if space[int(event.ydata), int(event.xdata)] == 1:
+            space[int(event.ydata), int(event.xdata)] = 0
+        else:
+            space[int(event.ydata), int(event.xdata)] = 1
+
+
+def pause(event):
+    global paused
+    if event.key == "control":
+        paused = not paused
+
+
 fig, ax = plt.subplots()
 im = plt.imshow(space)
 anim = animation.FuncAnimation(fig, update, cache_frame_data=False)
+paused = False
+fig.canvas.mpl_connect("button_press_event", onclick)
+fig.canvas.mpl_connect("key_press_event", pause)
 plt.show()
